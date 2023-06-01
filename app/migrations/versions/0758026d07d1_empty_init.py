f8666de7606787b2b049a5b3592ebf12eee4eaad
Job koech <jobkoech610@gmail.com>
2023-06-01 13:55:43 +0300

    completed migration

---
+++ app/migrations/versions/0758026d07d1_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 0758026d07d1
+Revises: 
+Create Date: 2023-06-01 13:12:03.758454
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0758026d07d1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
